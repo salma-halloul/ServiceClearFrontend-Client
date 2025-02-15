a23a82ca8761a647841858a8c0699d51e057e7ac
Salma Halloul <salmahalloul52@gmail.com>
2025-02-15 19:22:19 +0100

    persist data with redux

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,14 +1,14 @@
 "use client";
-
 import { Provider } from "react-redux";
-import store from "../redux/store";
-
+import store, { persistor } from "../redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 const ClientProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         {children}
+      </PersistGate>
     </Provider>
   );
 };
-
-export default ClientProvider;
+export default ClientProvider
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,7 +5,16 @@ import categoryReducer from "./slices/categorySlice";
 import reviewReducer from "./slices/reviewSlice";
 import contactReducer from "./slices/contactSlice";
 import quoteReducer from "./slices/quoteSlice";
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist/es/constants";
 const rootReducer = combineReducers({
   service: serviceReducer,
   category: categoryReducer,
@@ -13,14 +22,26 @@ const rootReducer = combineReducers({
   contact: contactReducer,
   quote: quoteReducer,
 });
-
+// Configuration de redux-persist
+const persistConfig = {
+  key: "root", // Clé de stockage
+  storage,     // Type de stockage (localStorage)
+  whitelist: [ "service"], // Persiste uniquement ces slices (optionnel)
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 // Configure le store
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER], // Actions redux-persist ignorées
+      },
+    }),
 });
+export const persistor = persistStore(store); // Crée le persistor
   
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-
 export default store;
